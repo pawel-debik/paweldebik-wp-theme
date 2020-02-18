b1d17b627ca32ce6b64cc7674002014754c44873
Pawel <paweldebik@gmail.com>
2020-02-18 11:56:51 +0100

    gulpfile shortcuts

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const cssnano = require('cssnano');
 
-function style(){
+function s(){
     // folder / files to look at
     return gulp.src('scss/*.scss')
     // do stuff
@@ -17,14 +17,14 @@ function style(){
     .pipe(gulp.dest('styles'))
 }
 
-function ugly(){
+function u(){
     return gulp.src('js/actions.js')
     .pipe(uglify())
     .pipe(rename("actions.min.js"))
     .pipe(gulp.dest('js'))
 };
 
-function conc(){
+function c(){
     return gulp.src(['./js/lazyload.min.js','./js/actions.min.js'])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('js'))
@@ -32,10 +32,10 @@ function conc(){
 
 function watch(){
     // gulp.watch('files-to-watch', gulp.series(['tasks', 'to', 'run']));
-    gulp.watch('scss/*.scss', gulp.series('style'));
+    gulp.watch('scss/*.scss', gulp.series('s'));
 };
 
-exports.style = style;
-exports.ugly = ugly;
-exports.conc = conc;
+exports.s = s;
+exports.u = u;
+exports.c = c;
 exports.watch = watch;
